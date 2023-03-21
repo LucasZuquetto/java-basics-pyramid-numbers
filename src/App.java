f0c00fe8c9a529d3e4903213150778a39e276552
LucasZuq <lucasdeozuquetto@hotmail.com>
2023-03-21 17:08:49 -0300

    algorithm done

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner inputNumber = new Scanner(System.in);
+        System.out.print("insira um número: ");
+        int number = inputNumber.nextInt();
+        for(int i = 1; i <= number; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
